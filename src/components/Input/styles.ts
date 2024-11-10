76971cef52fbdbde508de60b610836269d56b985
leobot336 <114504872+leobot336@users.noreply.github.com>
2024-11-09 22:13:34 -0300

    Update styles.ts

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,30 +1,32 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
+export const InputContainer = styled.div``;
+export const StyledInput = styled.input`
+  width: 300px;
+  height: 60px;
 
-export const InputContainer = styled.div`
-    width: 100%;
-    height: 42px;
-    color: #FFF;
+  border: none;
+  border-radius: 10px;
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
-    overflow: hidden;
-    padding: 0 10px;
-    
-    & input {
-        width: 100%;
-        height: 42px;
-        border-radius: 21px;
-        background-color: transparent;
-        border: 0;
-        outline: none;
-    }
+  margin: 10px;
 
-`
+  font-size: 19px;
+  color: #ffb703;
+  padding: 0 20px;
 
-export const ErrorMessage = styled.p`
-    color: red;
-    font-size:12px;
-    margin-top:8px;
-    margin-left:10px;
-`
+  &:: placeholder {
+    color: #ffb703;
+    opacity: 1;
+  }
+
+  &:focus {
+    outline: none; /* Remove o contorno padrão do navegador */
+  }
+`;
+
+export const ErrorMesage = styled.p`
+  color: red;
+  font-size: 12px;
+  margin-top: 8px;
+  margin-left: 10px;
+`;
