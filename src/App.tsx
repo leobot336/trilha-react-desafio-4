ab24b9fc361761d6d632d68c546dde9305376669
leobot336 <114504872+leobot336@users.noreply.github.com>
2024-11-09 22:16:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+         
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
